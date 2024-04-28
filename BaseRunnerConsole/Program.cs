ddd192ee4332f140c47d853d7c7109be991d8c00
Ryan <rhowell173@gmail.com>
2024-04-28 19:57:02 -0400

    Rename to Stolen Bases

--- BaseRunnerConsole/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using BaseRunnerLib;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-ConversionDB.SetConnectionString("Host=host.docker.internal;port=5432;Database=BaseRunnerDB;Username=postgres;Password=password");
-ConversionDB.TestConnection();
